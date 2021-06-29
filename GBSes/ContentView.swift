73deb4fa3f89d0c163e6e2c2d453d78e9bf94995
zlfn <elusive1102@naver.com>
2021-06-29 14:41:40 +0900

    TEST

---
+++ GBSes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GBSes
+//
+//  Created by 박찬웅 on 2021/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GBSes/GBSesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBSesApp.swift
+//  GBSes
+//
+//  Created by 박찬웅 on 2021/06/29.
+//
+
+import SwiftUI
+
+@main
+struct GBSesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
